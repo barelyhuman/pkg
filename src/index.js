60402b6143fa029fbe341cf9fa84870d5a803e3a
Reaper <ahoy@barelyhuman.dev>
2022-06-19 15:54:33 +0530

    base

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@
  */
 export async function moduleOne(param) {
   try {
-    console.log(param);
-    return true;
+    console.log(param)
+    return true
   } catch (err) {
-    return false;
+    return false
   }
 }
